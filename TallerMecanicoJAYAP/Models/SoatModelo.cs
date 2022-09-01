2f05ccb8355b7b533171d2f8ece84659269874c2
felipemedina05 <felipemedina05@gmail.com>
2022-08-31 23:15:32 -0500

    Se realiza correcion clase SoatModelo

--- TallerMecanicoJAYAP/Models/SoatModelo.cs
+++ TallerMecanicoJAYAP/Models/SoatModelo.cs
@@ -1,22 +1,23 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
-
-
-public class SoatModelo
+namespace TallerMecanicoJAYAP.Models
 {
-    public int IdSeguro { get; set; }
-    [Required(ErrorMessage = "EL campo es obligatorio")]//obligatoria
-    public string? TipoSeguro { get; set; }
-    [Required(ErrorMessage = "EL campo es obligatorio")]//obligatoria
-    public string? FechaCompra { get; set; }
-    [Required(ErrorMessage = "EL campo es obligatorio")]//obligatoria
-    public string? FechaVencimiento { get; set; }
-    [Required(ErrorMessage = "EL campo es obligatorio")]//obligatoria
+    public class SoatModelo
+    {
+        public int IdSeguro { get; set; }
+        [Required(ErrorMessage = "EL campo es obligatorio")]//obligatoria
+        public string? TipoSeguro { get; set; }
+        [Required(ErrorMessage = "EL campo es obligatorio")]//obligatoria
+        public string? FechaCompra { get; set; }
+        [Required(ErrorMessage = "EL campo es obligatorio")]//obligatoria
+        public string? FechaVencimiento { get; set; }
+        [Required(ErrorMessage = "EL campo es obligatorio")]//obligatoria
 
 
 
+    }
 }
+
 
 
 
